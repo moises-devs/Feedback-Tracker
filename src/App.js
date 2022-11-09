a88ae18c4e0565b0e01d9f097e9d8249b22d3b59
Moises Lopez <mozlopez@ucdavis.edu>
2022-11-09 13:10:53 -0800

    updated routing options

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import FeedbackBoard from "./Components/FeedbackBoard/FeedbackBoard";
 import FeedbackList from "./Components/FeedbackList/FeedbackList";
 import ReviewProvider from "./Context/ReviewProvider";
 import BgLayer from "./Components/UI/BgLayer/BgLayer";
-import NotFound from "./Pages/NotFound";
-import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <ReviewProvider>
     <Header/>
     <BgLayer>
-    <Routes>
-      <Route path="/" element={
-      <>
         <FeedbackBoard />
         <FeedbackList />
-      </>
-      }/>
-          <Route path="*" element={<NotFound/>}/>
-      </Routes>
     </BgLayer>
     </ReviewProvider>
   );
